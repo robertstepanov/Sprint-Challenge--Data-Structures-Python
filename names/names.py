769efbf6844c06f9850636d7e53ded29209857d7
Robert <robertdstepanov@gmail.com>
2020-05-15 14:04:22 -0400

    MVP

--- names/names.py
+++ names/names.py
@@ -1,8 +1,8 @@
 from binary_search_tree import BSTNode
 import time
-from collections import defaultdict
+
 start_time = time.time()
-# name = {}
+
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -14,37 +14,14 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 # Replace the nested for loops below with your improvements
 
-# for name_1 in names_1:
-#     if name_1 not in name:
-#         name[name_1] = 1
-#     else:
-#         if name[name_1] == 1:
-#             duplicates.append(name_1)
-#         name[name_1] += 1
+bst = BSTNode(names_1[0])
 
-# for name_2 in names_2:
-#     if name_2 not in name:
-#         name[name_2] = 1
-#     else:
-#         if name[name_2] == 1:
-#             duplicates.append(name_2)
-#         name[name_2] += 1
+for name_1 in names_1:
+    bst.insert(name_1)
 
-# bst = BSTNode('names')
-# for name in names_1:
-#     bst.insert(name)
-# for name_2 in names_2:
-#     if bst.contains(name_2):
-#         duplicates.append(name_2)
-
-bst = BSTNode('names')
-
-for names in names_1:
-    bst.insert(names)
-
-for names2 in names_2:
-    if bst.contains(names2):
-        duplicates.append(names2)
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 # ORIGINAL
 # for name_1 in names_1:
@@ -52,20 +29,6 @@ for names2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-
-# for name in names_1:
-#     name[name] += 1
-# for name in names_2:
-#     if name[name]:
-#         duplicates.append(name)
-
-# NOT SURE IF DEFAULTDICT IS CONSIDERED A DICT
-# l = defaultdict(int)
-# for name in names_1:
-#     l[name] += 1
-# for name in names_2:
-#     if l[name]:
-#         duplicates.append(name)
 
 # SETS NOT ALLOWED
 # duplicates = set(names_1) & set(names_2)
