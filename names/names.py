4c36fe7911ff3da8970fb31021c9dddc89de5491
Robert <robertdstepanov@gmail.com>
2020-05-15 13:07:09 -0400

    MVP

--- names/names.py
+++ names/names.py
@@ -1,7 +1,8 @@
 import time
-
+from collections import defaultdict
 start_time = time.time()
 
+name = {}
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -11,16 +12,50 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
-
 # Replace the nested for loops below with your improvements
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
+    if name_1 not in name:
+        name[name_1] = 1
+    else:
+        if name[name_1] == 1:
             duplicates.append(name_1)
+        name[name_1] += 1
+
+for name_2 in names_2:
+    if name_2 not in name:
+        name[name_2] = 1
+    else:
+        if name[name_2] == 1:
+            duplicates.append(name_2)
+        name[name_2] += 1
+
+# ORIGINAL
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# for name in names_1:
+#     name[name] += 1
+# for name in names_2:
+#     if name[name]:
+#         duplicates.append(name)
+
+# NOT SURE IF DEFAULTDICT IS CONSIDERED A DICT
+# l = defaultdict(int)
+# for name in names_1:
+#     l[name] += 1
+# for name in names_2:
+#     if l[name]:
+#         duplicates.append(name)
+
+# SETS NOT ALLOWED
+# duplicates = set(names_1) & set(names_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,39 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        # self.data = [None for i in range(capacity)]
+        self.capacity = capacity
+        self.current = 0
+
+        self.storage = [None] * capacity
 
     def append(self, item):
-        pass
+        # self.data.pop()
+        # self.data.append(item)
+        if self.capacity > len(self.storage):
+            self.storage.append(item)
+            self.current += 1
+        else:
+            self.storage[self.current] = item
+            self.current = ((self.current + 1) % self.capacity)
 
     def get(self):
-        pass
+        # return self.data
+
+        values = []
+        for item in self.storage:
+            if item is not None:
+                values.append(item)
+        return values
+
+
+buffer = RingBuffer(3)
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+print(buffer.get())
+buffer.append('d')
+print(buffer.get())
+buffer.append('e')
+buffer.append('f')
+print(buffer.get())
+print(buffer.get())
